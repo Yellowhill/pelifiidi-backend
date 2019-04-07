92f08c14a2b646f59672977cbf20b9fd87bf1e15
Yellowhill <jerekeltamaki@gmail.com>
2019-04-07 14:53:30 +0300

    datamodel update

--- src/utils/initializeData.js
+++ src/utils/initializeData.js
@@ -18,7 +18,7 @@ async function initializeLivegamers() {
 	const [lgExistsError, lgExists] = await to(
 		db.exists.Website({ name_in: ['Livegamers'] })
 	);
-	if (lgExistsError) return console.log('Error in initializeLivegamers: ', error);
+	if (lgExistsError) return console.log('Error in initializeLivegamers: ', lgExistsError);
 
 	if (lgExists) {
 		startPollingItems(getLgNews, lgInfo);
@@ -163,3 +163,36 @@ const dummyItems = [
 ];
 
 module.exports = initializeData;
+
+/**playground
+ * 
+ *
+ * 
+mutation {
+  createItem (
+    data:{
+  		title: "testi title",
+  		url: "testiurl1.com",
+  		publishDate: "2019-04-04T18:18:00.000Z",
+  		author: {
+        connect: {
+    			id: "5ca89c89a7b11b00076b4ec1"
+  		}}
+  		website: {
+    		connect: {
+      		id: "5ca89c89a7b11b00076b4ec0"
+    		}
+  		}
+  		textContent: {
+    		create: [{
+    			text: "diu diu",
+    			inlineLinks: {
+      		create: []
+    			}
+  			}]
+  	}
+    }) {id title}
+}
+}
+ * 
+ */
