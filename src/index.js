472263d6128b69d52feb0ebab21e8a49f2ca6844
Yellowhill <jerekeltamaki@gmail.com>
2019-08-09 17:11:35 +0300

    fixed subscriptions with apollo-server

--- src/index.js
+++ src/index.js
@@ -3,21 +3,35 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const jwt = require('jsonwebtoken');
+var cors = require('cors');
+const https = require('https');
+const http = require('http');
+
 const createServer = require('./utils/createServer');
 const initializeData = require('./utils/initializeData');
 
 const app = express();
 const path = '/graphql';
-const server = createServer();
-app.use(logger('dev'));
+const apollo = createServer();
+const server = http.createServer(app);
 
+app.use(logger('dev'));
+app.use(
+	cors({
+		origin: ['http://localhost:7777', 'ws://localhost:7777'],
+		credentials: true,
+		methods: ['GET', 'PUT', 'POST'],
+		// preflightContinue: true,
+		optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+	})
+);
 //Todo use express middleware to handle cookies
 app.use(cookieParser());
 
 // decode the JWT so we can get the user Id on each request
 app.use((req, res, next) => {
 	const { token } = req.cookies;
-	//console.log('TOKEN: ', token);
+	//console.log('TOKEN: ', token)
 	if (token) {
 		const { userId } = jwt.verify(token, process.env.APP_SECRET);
 		// put the userId onto the req for future requests to access
@@ -26,10 +40,12 @@ app.use((req, res, next) => {
 	next();
 });
 
-server.applyMiddleware({ app, path });
-app.listen({ port: 4444 }, () => {
+apollo.applyMiddleware({ app, cors: false });
+apollo.installSubscriptionHandlers(server);
+server.listen({ port: 4444 }, (diu) => {
 	initializeData();
-	console.log(`🚀 Server ready at AAAA http://localhost:4444${server.graphqlPath}`);
+	console.log('Diuuuu', apollo.subscriptionsPath);
+	console.log(`🚀 apollo ready at http://localhost:4444${apollo.graphqlPath} --`);
 });
 
 // server.start(
